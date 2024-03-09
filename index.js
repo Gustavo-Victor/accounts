cfa60ceb7488a315c94b4c4aed5a5a3d5707f400
= <=>
2024-03-09 19:52:17 -0300

    feat: create operations function

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 import chalk from "chalk"; 
 import inquirer from "inquirer"; 
+import fs from "node:fs"; 
 
+async function operation() {
+    try {
+        const response = await inquirer.prompt([
+            {
+                type: "list",
+                name: "action",
+                message: "What operation do you want to perform on the system?",
+                choices: [
+                    "Create account",
+                    "Check balance",
+                    "Deposit",
+                    "Withdraw",
+                    "Exit"
+                ]
+            }
+        ]); 
+
+        const { action } = response; 
+        console.log(action); 
+        
+    } catch (err) {
+        console.log(err); 
+    }
+}
+
+operation(); 
