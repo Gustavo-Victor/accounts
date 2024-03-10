974445f1f2257ffc1d86230fa7e909bbddc9551f
= <=>
2024-03-10 12:31:49 -0300

    feat: add withdraw functionality

--- index.js
+++ index.js
@@ -43,6 +43,7 @@ async function operation() {
                 deposit();
             break;
             case WITHDRAW: 
+                withdraw(); 
             break;
             case EXIT:
                 console.log(chalk.bgBlue.black("Thank you for using Accounts."));
@@ -76,9 +77,8 @@ async function buildAccount() {
 
         if(!accountName) {
             // throw new Error("Account name is required."); 
-            console.log(chalk.bgRed.black("Account name is required")); 
-            buildAccount();
-            return ;
+            console.log(chalk.bgRed.black("Account name is required.")); 
+            return buildAccount(); 
         }
         
         console.info(accountName); 
@@ -89,8 +89,7 @@ async function buildAccount() {
 
         if(fs.existsSync(`./user_accounts/${accountName}.json`)) {
             console.log(chalk.bgRed.black("This account already exists. Choose another name."));
-            buildAccount(); 
-            return ;
+            return buildAccount(); 
         }
 
         fs.writeFileSync(`./user_accounts/${accountName}.json`, `{"balance": 0}`, (err) => {
@@ -104,7 +103,6 @@ async function buildAccount() {
         
     } catch (error) {
         console.log(chalk.bgRed.black(error));
-
     }
 } 
 
@@ -126,17 +124,15 @@ async function getAccountBalance() {
             return getAccountBalance(); 
         }
 
-        const accountInfo = getAccount(accountName); 
-
-        if(!accountInfo) {
-            console.log(chalk.bgRed.black("This account does not exist."));
+        if(!accountExists(accountName)) {
             return operation(); 
         }
-        
+
+        const accountInfo = getAccount(accountName); 
         const { balance } = accountInfo; 
 
         console.log(chalk.bgBlue.black(`The current balance is $${parseFloat(balance)}`));
-        operation(); 
+        return operation(); 
     } catch (error) {
         console.log(chalk.bgRed.black(error)); 
     }
@@ -161,7 +157,7 @@ async function deposit() {
         }
 
         if(!accountExists(accountName)) {
-            return deposit(); 
+            return operation(); 
         }
 
         addAmount(accountName); 
@@ -172,12 +168,83 @@ async function deposit() {
 
 
 // withdraw
+async function withdraw() {
+    try {
+        const response = await inquirer.prompt([
+            {
+                name: "accountName",
+                message: "Enter the name of the account: "
+            }
+        ]); 
+        const { accountName } = response; 
+
+        if(!accountName) {
+            console.log(chalk.bgRed("Account name is required.")); 
+            return withdraw(); 
+        }
+
+        if(!accountExists(accountName)) {
+            return operation(); 
+        }
+
+        removeAmount(accountName);         
+
+    } catch (error) {
+        console.log(chalk.bgRed.black(error)); 
+    }
+}
+
+
+// remove amount from account
+async function removeAmount(accountName) {
+    try {
+        const accountInfo = getAccount(accountName); 
+        const withdrawResponse = await inquirer.prompt([
+            {
+                name: "amount",
+                message: "How much would you like to withdraw?"
+            }
+        ]); 
+
+        const { amount } = withdrawResponse; 
+
+        if(!amount) {
+            console.log(chalk.bgRed.black("Amount is required."));
+            return removeAmount(accountName); 
+        }
+
+        if(isNaN(Number(amount))) {
+            console.log(chalk.bgRed.black("Amount must be a number.")); 
+            return removeAmount(accountName); 
+        }
+
+        const { balance } = accountInfo; 
+        if(parseFloat(balance) < parseFloat(amount)) {
+            console.log(chalk.bgRed.black("Insufficient funds."));
+            return removeAmount(accountName); 
+        }
+
+        accountInfo.balance = parseFloat(balance) - parseFloat(amount); 
+        const accountJSON = JSON.stringify(accountInfo); 
+
+        fs.writeFileSync(`./user_accounts/${accountName}.json`, accountJSON, (err) => {
+            if(err) {
+                throw new Error("Failed to withdraw."); 
+            }
+        })
+    
+        console.log(chalk.green(`$${parseFloat(amount)} was withdrawn from the account.`));
+        return operation(); 
+    } catch (error) {
+        console.log(chalk.bgRed.black(error));
+    }
+}
 
 
 // check if account exists
 function accountExists(accountName) {
     if(!fs.existsSync(`./user_accounts/${accountName}.json`)) {
-        console.log(chalk.bgRed.black("This account does not exist. choose another name!")); 
+        console.log(chalk.bgRed.black("This account does not exist. Choose another name!")); 
         return false; 
     } 
     return true; 
